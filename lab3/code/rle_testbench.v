8ec3482d41890e6c3073a0e3c23a7f2aa61f1c3b
Joseph Cao <jkcao@umass.edu>
2017-03-23 16:19:37 -0400

    added and verrified more test vectors

--- lab3/code/rle_testbench.v
+++ lab3/code/rle_testbench.v
@@ -39,20 +39,27 @@ module rle_testbench;
 	#1 rstt=1'b1;
 	#2 rstt=1'b0;
 	// Reset signal assignment
-
 	end
 
 	// test vector generator
 	initial
 	begin
-	  // test vector 1
-	#1 in_datat[7:0]= 8'b11110111;
-	//#64 end_of_streamt= 1'b1;
-	#64 in_datat[7:0]=8'b10101111;
-	// test vector 2
- 	// test vector n
-
-
+	//test vector 1
+	#1 in_datat[7:0]= 8'b00000000;
+	//test vector 2
+	#42 in_datat[7:0]=8'b11111111;
+	//test vector 3
+	#46 in_datat[7:0]=8'b01010101; //worse case
+	//test vector 4
+        #109 in_datat[7:0]=8'b11111110;
+	//test vector 5
+	#49 in_datat[7:0]=8'b10000000;
+	//test vector 6
+	#57 in_datat[7:0]=8'b00001111;
+	//test vector 7
+	#48 in_datat[7:0]=8'b01111110;
+	//test vector 8
+	#58 in_datat[7:0]=8'b11001100;
 	$stop;
 	end
 endmodule
